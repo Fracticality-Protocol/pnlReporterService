1a672e505bac4146373583e489f1673622e05992
dylmanning <dyl.manning@gmail.com>
2024-08-30 15:33:54 +1000

    Move FractalityPnlReporter to global scope for lambda function

--- src/handler.ts
+++ src/handler.ts
@@ -1,19 +1,20 @@
 import { type Context, type Handler } from 'aws-lambda'
-import { FractalityPnlReporter } from './pnlReporter'
+
 import FractalityV2VaultABI from '../contracts/FractalityV2Vault.json'
+import { FractalityPnlReporter } from './pnlReporter'
 import { env } from './env'
 
 // NOTE: no event required yet
 // interface LambdaEvent {}
 
-export const handler: Handler<void, void> = async (event, context: Context): Promise<void> => {
-  const pnlReporter = new FractalityPnlReporter(
-    env,
-    FractalityV2VaultABI.abi,
-    env.OPERATION_MODE,
-    env.KEY_MODE
-  )
+const pnlReporter = new FractalityPnlReporter(
+  env,
+  FractalityV2VaultABI.abi,
+  env.OPERATION_MODE,
+  env.KEY_MODE
+)
 
+export const handler: Handler<void, void> = async (event, context: Context): Promise<void> => {
   try {
     await pnlReporter.initialize()
   } catch (error) {

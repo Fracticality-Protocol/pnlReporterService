a24ab89fa67f3f4db533f998c8ee1595467aafb7
dylmanning <dyl.manning@gmail.com>
2024-09-09 15:25:23 +1000

    Decouple terrafrom infra

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -22,8 +22,28 @@ resource "aws_iam_policy" "kms_policy" {
           "kms:DescribeKey",
         ]
         Effect   = "Allow"
+        Resource = data.aws_kms_key.mainnet.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "test_kms_policy" {
+  name        = "${var.name}-kms-decrypt-policy-test"
+  description = "IAM policy for ${var.name} to decrypt using TEST KMS key"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "kms:Decrypt",
+          "kms:DescribeKey",
+        ]
+        Effect   = "Allow"
         Resource = data.aws_kms_key.testnet.arn
       }
     ]
   })
 }
+
